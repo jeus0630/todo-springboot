8a11cc0f50f41a66d0d8e43ea78fb72ce6efa9a5
jewoo <jeus0630@gmail.com>
2022-09-09 23:53:54 +0900

    Add: Lombok

---
+++ src/main/java/org/example/model/TodoEntity.java
@@ -0,0 +1,16 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoEntity {
+
+
+}
